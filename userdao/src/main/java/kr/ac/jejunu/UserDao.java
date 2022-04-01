be1631411d336da738808c851dcb94a203c53423
seminify <seminify@gmail.com>
2022-04-01 22:25:03 +0900

    update, delete

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -10,51 +10,141 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public User findById(Integer id) throws ClassNotFoundException, SQLException {
-//        드라이버 로딩
-        Connection connection = dataSource.getConnection();
-//        sql 작성
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(
-                        "select * from userinfo where id = ?"
-                );
-        preparedStatement.setInt(1, id);
-//        sql 실행
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-//        User 에 데이터 매핑
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-//        자원 해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+    public User findById(Integer id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try {
+//            드라이버 로딩
+            connection = dataSource.getConnection();
+//            sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "select * from userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+//            sql 실행
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+//                User 에 데이터 매핑
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } finally {
+//            자원 해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 //        User 리턴
         return user;
     }
 
-    public void insert(User user) throws ClassNotFoundException, SQLException {
-//        드라이버 로딩
-        Connection connection = dataSource.getConnection();
-//        sql 작성
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(
-                        "insert into userinfo(name, password) values (?, ?)",
-                        Statement.RETURN_GENERATED_KEYS
-                );
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.executeUpdate();
-//        sql 실행
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-//        User 에 데이터 매핑
-        user.setId(resultSet.getInt(1));
-//        자원 해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+    public void insert(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+//            드라이버 로딩
+            connection = dataSource.getConnection();
+//            sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "insert into userinfo(name, password) values (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
+//            sql 실행
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+//            User 에 데이터 매핑
+            user.setId(resultSet.getInt(1));
+        } finally {
+//            자원 해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+//            드라이버 로딩
+            connection = dataSource.getConnection();
+//            sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "update userinfo set name = ?, password = ? where id = ?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            preparedStatement.executeUpdate();
+        } finally {
+//            자원 해지
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delete(Integer id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+//            드라이버 로딩
+            connection = dataSource.getConnection();
+//            sql 작성
+            preparedStatement = connection.prepareStatement(
+                    "delete from userinfo where id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } finally {
+//            자원 해지
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- userdao/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class UserDaoTests {
     private static UserDao userDao;
@@ -26,8 +25,8 @@ public class UserDaoTests {
         Integer id = 1;
         String name = "hulk";
         String password = "1234";
-
         User user = userDao.findById(id);
+
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
@@ -37,18 +36,49 @@ public class UserDaoTests {
     public void insert() throws SQLException, ClassNotFoundException {
         String name = "hulk";
         String password = "1111";
-
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-
-
         userDao.insert(user);
 
         assertThat(user.getId(), greaterThan(0));
-
         User insertedUser = userDao.findById(user.getId());
         assertThat(insertedUser.getName(), is(user.getName()));
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void update() throws SQLException {
+//        사용자 추가
+        String name = "hulk";
+        String password = "1111";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+//        수정
+        String updatedName = "허윤호";
+        String updatedPassword = "1111";
+        user.setName(updatedName);
+        user.setPassword(updatedPassword);
+        userDao.update(user);
+
+        User updatedUser = userDao.findById(user.getId());
+        assertThat(updatedUser.getName(), is(updatedName));
+        assertThat(updatedUser.getPassword(), is(updatedPassword));
+    }
+
+    @Test
+    public void delete() throws SQLException {
+//        사용자 추가
+        User user = new User();
+        user.setName("hulk");
+        user.setPassword("1234");
+        userDao.insert(user);
+//        삭제
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.findById(user.getId());
+        assertThat(deletedUser, nullValue());
     }
 }
