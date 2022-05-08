13b8055940c2d9e0dc9da09b120a97654ccd556f
seminify <seminify@gmail.com>
2022-05-09 07:58:42 +0900

    servlet filter, listener

--- userdao/src/main/java/kr/ac/jejunu/UserServlet.java
+++ userdao/src/main/java/kr/ac/jejunu/UserServlet.java
@@ -8,19 +8,22 @@ import java.io.IOException;
 
 public class UserServlet extends GenericServlet {
     @Override
-    public void init() throws ServletException {
-        System.out.println("************ init *****************");
-        super.init();
-    }
-
-    @Override
     public void destroy() {
-        System.out.println("************ destory *****************");
+        System.out.println("********* destroy ***********");
         super.destroy();
     }
 
     @Override
+    public void init() throws ServletException {
+        System.out.println("*********  init ***********");
+        super.init();
+    }
+
+    @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("************ service *****************");
+        res.setContentType("text/html;charset=UTF-8");
+        String html = "<html><h1>Hi 허윤호</h1></html>";
+        res.getWriter().println(html);
+        System.out.println("********* service ***********");
     }
 }
