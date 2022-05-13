74c880441825b5dbe0f61a894da286e976a566a3
seminify <seminify@gmail.com>
2022-05-13 17:28:07 +0900

    file upload

--- userdao/src/main/java/kr/ac/jejunu/UserController.java
+++ userdao/src/main/java/kr/ac/jejunu/UserController.java
@@ -3,25 +3,49 @@ package kr.ac.jejunu;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+
 @Controller
-@RequestMapping("/user2")
+@RequestMapping("/user")
 public class UserController {
     @RequestMapping
-    public ModelAndView user(@RequestParam("id") Integer id, @RequestParam("name") String name){
-        User user=new User();
+    public ModelAndView user(@RequestParam("id") Integer id, @RequestParam("name") String name) {
+        User user = new User();
         user.setId(id);
         user.setName(name);
-        ModelAndView modelAndView=new ModelAndView("user");
+        ModelAndView modelAndView = new ModelAndView("user");
         modelAndView.addObject(user);
         return modelAndView;
     }
+
     @ExceptionHandler(Exception.class)
-    public ModelAndView error(Exception e){
+    public ModelAndView error(Exception e) {
         ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("e",e);
+        modelAndView.addObject("e", e);
+        return modelAndView;
+    }
+
+    @RequestMapping("/upload")
+    public void upload() {
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
+        File path = new File(request.getServletContext()
+                .getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView("upload");
+        modelAndView.addObject("url"
+                , "/images/" + file.getOriginalFilename());
         return modelAndView;
     }
 }
