22475935ad68616c1d3de2a121e28834db715f01
seminify <seminify@gmail.com>
2022-06-09 13:53:33 +0900

    spring boot

--- user/src/main/java/kr/ac/jejunu/user/User.java
+++ user/src/main/java/kr/ac/jejunu/user/User.java
@@ -1,15 +1,17 @@
 package kr.ac.jejunu.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Data;
+
+@Entity(name = "userinfo")
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String password;
---
+++ user/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -0,0 +1,7 @@
+package kr.ac.jejunu.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+
+}
